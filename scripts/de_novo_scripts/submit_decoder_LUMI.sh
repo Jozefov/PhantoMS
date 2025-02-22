0ffc7b855ead6699bbee9de579ec6c5ecee9dc7a
Jozefov <mn.mastaf@gmail.com>
2025-02-22 15:19:49 +0100

    decoder lumi submit

--- scripts/de_novo_scripts/submit_decoder_LUMI.sh
+++ scripts/de_novo_scripts/submit_decoder_LUMI.sh
@@ -8,18 +8,14 @@
 #SBATCH --mem=480G
 #SBATCH --time=24:00:00
 
-# Ensure WANDB API key is available.
 export WANDB_API_KEY=${WANDB_API_KEY}
 
-# Change to the project directory.
 cd /scratch/project_465001738/jozefov_147/PhantoMS
 
-# Load necessary modules and activate your environment.
 module --force purge
 module use /appl/local/csc/modulefiles/
 module load pytorch/2.4
 export PYTHONNOUSERSITE=1
 source phantoms_venv/bin/activate
 
-# Launch the decoder training script using distributed training.
 srun python3 -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 scripts/de_novo_scripts/run_decoder_train_LUMI.py /scratch/project_465001738/jozefov_147/PhantoMS/phantoms/models/denovo/configs_server/config_decoder_LUMI.yml
